717e745ffca916fe0a042199ffa90e45527bf184
Clint Moore <clintmoore@yahoo.com>
2016-04-29 07:58:13 -0500

    Initialize repo

---
+++ server.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var app = express();
+var PORT = 3000;
+
+var middleware = {
+    requireAuthentication: function (req, res, next) {
+        console.log('Private route hit!');
+        next();
+    },
+    logger: function(req,res, next){
+        var timestamp = new Date().toString();
+        console.log('Request:[' + timestamp + ']: ' + req.method + ' ' + req.originalUrl);
+        next();
+    } 
+};
+
+//app.use(middleware.requireAuthentication);
+app.use(middleware.logger);
+
+app.get('/about', middleware.requireAuthentication, function(req,res){
+    res.send('About us');
+});
+
+app.use(express.static(__dirname + '/public')); //exposes entire directory
+
+app.listen(PORT, function (){
+    console.log('Server is running on port:' + PORT);
+});
