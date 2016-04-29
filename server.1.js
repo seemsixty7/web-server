34405572054efb8cbe7f99237f5c9d1816f7cb02
Clint Moore <clintmoore@yahoo.com>
2016-04-29 08:20:47 -0500

    Refactored Middleware

--- server.1.js
+++ server.1.js
@@ -2,7 +2,17 @@ var express = require('express');
 var app = express();
 var PORT = 3000;
 
-var middleware = require('./middleware');
+var middleware = {
+    requireAuthentication: function (req, res, next) {
+        console.log('Private route hit!');
+        next();
+    },
+    logger: function(req,res, next){
+        //var timestamp = new Date().toString();
+        console.log('Request:[' + new Date().toString() + ']: ' + req.method + ' ' + req.originalUrl);
+        next();
+    } 
+};
 
 //app.use(middleware.requireAuthentication);
 app.use(middleware.logger);
